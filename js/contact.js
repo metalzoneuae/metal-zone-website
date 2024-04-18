602bc1f51e7fe10d88900c4b70026169427242ee
Abdul Samad M J <samadmj4@gmail.com>
2024-04-18 16:58:52 +0400

    Contact Us page updated, formm submission added

--- js/contact.js
+++ js/contact.js
@@ -1,45 +1,20 @@
 /**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
+ * WEBSITE: https://metazoneuae.com
+ * GITHUB: https://github.com/ennovater/
  */
 
-(function ($) {
-  "use strict";
 
-  var form = $(".contact__form"),
-    message = $(".contact__msg"),
-    form_data;
-
-  // Success function
-  function done_func(response) {
-    message.fadeIn().removeClass("alert-danger").addClass("alert-success");
-    message.text(response);
-    setTimeout(function () {
-      message.fadeOut();
-    }, 2000);
-    form.find('input:not([type="submit"]), textarea').val("");
+var submitted = false;
+const contactUs = () => {
+  if (submitted) {
+    const ok = Swal.fire({
+      title: "Email Sent Successfully",
+      text: "We will get back to you, Thank You.",
+      icon: "success",
+      confirmButtonText: "OK",
+    }).then(() => {
+      window.location = "/";
+    });
   }
+};
 
-  // fail function
-  function fail_func(data) {
-    message.fadeIn().removeClass("alert-success").addClass("alert-success");
-    message.text(data.responseText);
-    setTimeout(function () {
-      message.fadeOut();
-    }, 2000);
-  }
-
-  form.submit(function (e) {
-    e.preventDefault();
-    form_data = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: form.attr("action"),
-      data: form_data,
-    })
-      .done(done_func)
-      .fail(fail_func);
-  });
-})(jQuery);
